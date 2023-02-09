d79ceed43f6875432cfb19477b2f81deeecd402e
Yoshiyuki Kitaguchi <kitaguchi@yk-gankeisei.com>
2023-02-10 02:14:08 +0900

    Initial Commit

---
+++ GravAI_ios/GravAI_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GravAI_iosApp.swift
+//  GravAI_ios
+//
+//  Created by Yoshiyuki Kitaguchi on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct GravAI_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
